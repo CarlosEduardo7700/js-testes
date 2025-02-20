5be6b97da42d8d670805dacc25dd501196df8877
CarlosEduardo7700 <carlos.msilva@quintoandar.com.br>
2025-02-20 16:47:53 -0300

    feat: Gerando relatórios dos testes

--- tests/folhaDePagamento.test.js
+++ tests/folhaDePagamento.test.js
@@ -1,16 +1,18 @@
 /* eslint-disable no-undef */
 import { somaHorasExtras, calculaDescontos } from "../index.js";
 
-test("Deve retornar a soma das horas extras", () => {
-  const esperado = 2500;
-  const retornado = somaHorasExtras(2000, 500);
-
-  expect(retornado).toBe(esperado);
-});
-
-test("Deve retornar o novo valor com o desconto", () => {
-  const esperado = 2300;
-  const retornado = calculaDescontos(2500, 200);
-
-  expect(retornado).toBe(esperado);
+describe("Testes dos cáculos de folha", () => {
+  it("Deve retornar a soma das horas extras", () => {
+    const esperado = 2500;
+    const retornado = somaHorasExtras(2000, 500);
+    
+    expect(retornado).toBe(esperado);
+  });
+    
+  it("Deve retornar o novo valor com o desconto", () => {
+    const esperado = 2300;
+    const retornado = calculaDescontos(2500, 200);
+    
+    expect(retornado).toBe(esperado);
+  });
 });
